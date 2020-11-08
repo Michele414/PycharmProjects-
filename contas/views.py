661e0de88513a3bdaa1508ddff2c2b44c1c1239b
Michele414 <miih414@gmail.com>
2020-11-07 22:53:05 -0300

    sem bug

--- contas/views.py
+++ contas/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from datetime  import datetime 
+from .models import transaçao
+import datetime 
 
 
-# Create your views here.
-def home (request):
-  #data = {}
- # data['now'] = datetime.datetime.now()
- # html = "<html><body>It is now %s.</body></html>" % now
- return render (request,'contas/home.html',)
+ #Create your views here.
 
-        
+def  home (request):
+     data = {}
+
+     return render (request,'contas/home.html',data)
+
+def listagem(request):
+  data = {}
+  data["transaçoes"]= transaçao.objects.all()
+  return render (request,'contas/listagem.html',data)
+      
